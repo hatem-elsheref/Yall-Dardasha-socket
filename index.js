984bb365de3ea5bcc1116f322b57e5fc027ac7c3
hatem-elsheref <hatem_mohamed_elsheref@yahoo.com>
2021-07-06 13:13:20 +0200

    fix end room in scoket.io api old version api

--- index.js
+++ index.js
@@ -1,93 +1,92 @@
 const os = require('os');
 const server = require('http').createServer();
 const io = require("socket.io")(server)
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
 
 
-    socket.on('create', function (info){
-        try{
+    socket.on('create', function (info) {
+        try {
             const data = JSON.parse(info)
             socket.join(data.roomId)
-            socket.emit('room_created', {message: 'room created successfully'})
-        }catch(e){
-            socket.emit('error', {message: 'couldn\'t perform requested action'});
+            socket.emit('room_created', { message: 'room created successfully' })
+        } catch (e) {
+            socket.emit('error', { message: 'couldn\'t perform requested action' });
         }
     })
 
-    socket.on('join', function (info){
-        try{
+    socket.on('join', function (info) {
+        try {
             const data = JSON.parse(info)
             socket.join(data.roomId)
-            socket.emit('joined_room',  {message: 'joined successfully'})
-            io.to(data.roomId).emit('refresh', {message: 'new member joined so try to refresh the view'})
-        }catch(e){
-            socket.emit('error', {message: 'couldn\'t perform requested action'});
+            socket.emit('joined_room', { message: 'joined successfully' })
+            io.to(data.roomId).emit('refresh', { message: 'new member joined so try to refresh the view' })
+        } catch (e) {
+            socket.emit('error', { message: 'couldn\'t perform requested action' });
         }
     })
 
-    socket.on('leave',function(info){
-        try{
+    socket.on('leave', function (info) {
+        try {
             const data = JSON.parse(info)
             socket.leave(data.roomId);
-            io.to(data.roomId).emit('refresh', {message: "there is member left the room so try to refresh the view"});
-        }catch(e){
-            socket.emit('error', {message: 'couldn\'t perform requested action'});
+            io.to(data.roomId).emit('refresh', { message: "there is member left the room so try to refresh the view" });
+        } catch (e) {
+            socket.emit('error', { message: 'couldn\'t perform requested action' });
         }
     })
 
-    socket.on('want_to_speak', function(info) {
+    socket.on('want_to_speak', function (info) {
         const data = JSON.parse(info)
         // moderator or room admin only can see this event
-        socket.to(data.roomId).emit('listen_if_member_want_to_speak', {username: data.username, socketId: io.sockets.sessionId});
+        socket.to(data.roomId).emit('listen_if_member_want_to_speak', { username: data.username, socketId: io.sockets.sessionId });
     })
 
-    socket.on('allow_member_to_speak', function(info) {
+    socket.on('allow_member_to_speak', function (info) {
         const data = JSON.parse(info)
         // the member who try to rise hand to speak => change state from audience to speaker
-        socket.to(data.memberSocketId).emit('if_i_can_speak', {message: "you are speaker now"});
-        io.to(data.roomId).emit('refresh', {message: "there is member changed his state from audience to speaker so try to refresh the view"});
+        socket.to(data.memberSocketId).emit('if_i_can_speak', { message: "you are speaker now" });
+        io.to(data.roomId).emit('refresh', { message: "there is member changed his state from audience to speaker so try to refresh the view" });
     })
 
-    socket.on('dis_allow_member_to_speak', function(info) {
+    socket.on('dis_allow_member_to_speak', function (info) {
         const data = JSON.parse(info)
         // moderator or room admin only can see this event
-        socket.to(data.memberSocketId).emit('listen_if_moderator_cancel_my_request', {message: "your request to be speaker refused by admin"});
+        socket.to(data.memberSocketId).emit('listen_if_moderator_cancel_my_request', { message: "your request to be speaker refused by admin" });
     })
 
-    socket.on('move_to_audience', function(info) {
+    socket.on('move_to_audience', function (info) {
         const data = JSON.parse(info)
-        io.to(data.roomId).emit('refresh', {message: "there is a member moved to audience"});
+        io.to(data.roomId).emit('refresh', { message: "there is a member moved to audience" });
     })
 
 
-    socket.on('member_open_mic', function(info) {
+    socket.on('member_open_mic', function (info) {
         const data = JSON.parse(info)
-        io.to(data.roomId).emit('refresh_mic', {message: "there is a member opened his mic", userId: data.userId});
+        io.to(data.roomId).emit('refresh_mic', { message: "there is a member opened his mic", userId: data.userId });
     })
 
-    socket.on('member_closed_mic', function(info) {
+    socket.on('member_closed_mic', function (info) {
         const data = JSON.parse(info)
-        io.to(data.roomId).emit('refresh_mic', {message: "there is a member closed his mic", userId: data.userId});
+        io.to(data.roomId).emit('refresh_mic', { message: "there is a member closed his mic", userId: data.userId });
     })
 
 
-    socket.on('end_room', function(info){
+    socket.on('end_room', function (info) {
         let data = JSON.parse(info)
-        io.in(data.roomId).clients(function(error, socketIds){
-          if(error){
-              socket.emmit('no_thing_happend', {message: 'some thing went wrong in server so -> stay every thing at old state'})
-              throw error
-          }
-        socket.to(data.roomId).emit('room_ended', {message : 'admin ended the room so -> android dev  make alert and then go back and refresh to show all rooms '})
-        socket.emit('my_room_ended', {message: "you closed the room so -> android dev make alert and then go back and refresh to show all rooms "})
-        socketIds.forEach(function(socketId){io.sockets.sockets[socketId].leave(data.roomId);});
-        });
+        try {
+            socket.to(data.roomId).emit('room_ended', { message: 'admin ended the room so -> android dev  make alert and then go back and refresh to show all rooms ' })
+            io.socketsLeave(data.roomId);
+            socket.emit('my_room_ended', { message: "you closed the room so -> android dev make alert and then go back and refresh to show all rooms " })
+        } catch (error) {
+            socket.emmit('no_thing_happend', { message: 'some thing went wrong in server so -> stay every thing at old state' })
+            throw error
+        }
     });
 
-    socket.on('ipaddr', function() {
+    socket.on('ipaddr', function () {
         const ifaces = os.networkInterfaces();
         for (const dev in ifaces) {
-            ifaces[dev].forEach(function(details) {
+            ifaces[dev].forEach(function (details) {
                 if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
                     socket.emit('ipaddr', details.address);
                 }
